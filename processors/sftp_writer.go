7e2e342bc18932224baa5c79ffeee736807344c9
pcrawfor <pcrawfor@gmail.com>
2015-11-12 15:54:49 -0700

    Refactor sftp writer - don't open the network connection and create the file until we start recv'ing data

--- processors/sftp_writer.go
+++ processors/sftp_writer.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/DailyBurn/ratchet/data"
 	"github.com/DailyBurn/ratchet/logger"
+	"github.com/DailyBurn/ratchet/util"
 	"github.com/pkg/sftp"
 )
 
@@ -13,29 +14,36 @@ type SftpWriter struct {
 	ftpFilepath string
 	client      *sftp.Client
 	file        *sftp.File
+	params      *parameters
+	initialized bool
+}
+
+type parameters struct {
+	server   string
+	username string
+	password string
+	path     string
 }
 
 // NewSftpWriter instantiates a new writer and opens a connection to the remote server creating the file referenced in the path
 func NewSftpWriter(server, username, password, path string) *SftpWriter {
-	c, e := connect(server, username, password)
+	return &SftpWriter{params: &parameters{server, username, password, path}, initialized: false}
+}
+
+func (w *SftpWriter) init(killChan chan error) {
+	c := connect(w.params.server, w.params.username, w.params.password, killChan)
+
+	f, e := c.Create(w.params.path)
 	if e != nil {
-		logger.Error("Error instantiating ftp writer", e)
-		return nil
+		util.KillPipelineIfErr(e, killChan)
 	}
-
-	file, e := c.Create(path)
-	if e != nil {
-		logger.Error("Error instantiating ftp writer", e)
-		return nil
-	}
-
-	f := SftpWriter{file: file, client: c}
-
-	return &f
+	w.client = c
+	w.file = f
+	w.initialized = true
 }
 
 // connect opens an ssh connection and instantiates an sftp client based on that connection
-func connect(server, username, password string) (*sftp.Client, error) {
+func connect(server, username, password string, killChan chan error) *sftp.Client {
 	// open ssh connection
 	config := &ssh.ClientConfig{
 		User: username,
@@ -43,23 +51,26 @@ func connect(server, username, password string) (*sftp.Client, error) {
 			ssh.Password(password),
 		},
 	}
-	c, err := ssh.Dial("tcp", server, config)
-	if err != nil {
-		return nil, err
+	c, e := ssh.Dial("tcp", server, config)
+	if e != nil {
+		util.KillPipelineIfErr(e, killChan)
 	}
 
 	// instantiate sftp client
-	sftp, err := sftp.NewClient(c)
-	if err != nil {
-		return nil, err
+	sftp, e := sftp.NewClient(c)
+	if e != nil {
+		util.KillPipelineIfErr(e, killChan)
 	}
 
-	return sftp, nil
+	return sftp
 }
 
 // ProcessData writes data as is directly to the output file
 func (w *SftpWriter) ProcessData(d data.JSON, outputChan chan data.JSON, killChan chan error) {
 	logger.Debug("FTPWriter Process data:", string(d))
+	if !w.initialized {
+		w.init(killChan)
+	}
 	w.file.Write([]byte(d))
 }
 
